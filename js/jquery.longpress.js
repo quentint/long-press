61e80f6c2bfe229510adedb966e81d241ce93185
azar <azar.career@gmail.com>
2019-10-22 12:54:27 +0530

    Added factory method

--- js/jquery.longpress.js
+++ js/jquery.longpress.js
@@ -3,8 +3,7 @@
  *  Description: Pops a list of alternate characters when a key is long-pressed
  *  Author: Quentin Thiaucourt, http://toki-woki.net
  *	Licence: MIT License http://opensource.org/licenses/mit-license.php
- */
-;(function (factory) {
+ */;(function (factory) {
 	if (typeof define === "function" && define.amd) {
 		// AMD. Register as an anonymous module depending on jQuery.
 		define(["jquery"], factory , window);
@@ -17,87 +16,87 @@
 	}
 })(function ($, window, undefined) {
     
-	var pluginName = 'longPress',
-		document = window.document,
-		defaults = {/*
+    var pluginName = 'longPress',
+        document = window.document,
+        defaults = {/*
 	        propertyName: "value"
         */};
 
-	var moreChars = {
+	var moreChars={
 		// extended latin (and african latin)
 		// upper
-		'A': 'ĀĂÀÁÂÃÄÅĄⱭ∀Æ',
-		'B': 'Ɓ',
-		'C': 'ÇĆĈĊČƆ',
-		'D': 'ÐĎĐḎƊ',
-		'E': 'ÈÉÊËĒĖĘẸĚƏÆƎƐ€',
-		'F': 'ƑƩ',
-		'G': 'ĜĞĠĢƢ',
-		'H': 'ĤĦ',
-		'I': 'ÌÍÎÏĪĮỊİIƗĲ',
-		'J': 'ĴĲ',
-		'K': 'ĶƘ',
-		'L': 'ĹĻĽŁΛ',
-		'N': 'ÑŃŅŇŊƝ₦',
-		'O': 'ÒÓÔÕÖŌØŐŒƠƟ',
-		'P': 'Ƥ¶',
-		'R': 'ŔŘɌⱤ',
-		'S': 'ßſŚŜŞṢŠÞ§',
-		'T': 'ŢŤṮƬƮ',
-		'U': 'ÙÚÛÜŪŬŮŰŲɄƯƱ',
-		'V': 'Ʋ',
-		'W': 'ŴẄΩ',
-		'Y': 'ÝŶŸƔƳ',
-		'Z': 'ŹŻŽƵƷẔ',
+		'A':'ĀĂÀÁÂÃÄÅĄⱭ∀Æ',
+		'B':'Ɓ',
+		'C':'ÇĆĈĊČƆ',
+		'D':'ÐĎĐḎƊ',
+		'E':'ÈÉÊËĒĖĘẸĚƏÆƎƐ€',
+		'F':'ƑƩ',
+		'G':'ĜĞĠĢƢ',
+		'H':'ĤĦ',
+		'I':'ÌÍÎÏĪĮỊİIƗĲ',
+		'J':'ĴĲ',
+		'K':'ĶƘ',
+		'L':'ĹĻĽŁΛ',
+		'N':'ÑŃŅŇŊƝ₦',
+		'O':'ÒÓÔÕÖŌØŐŒƠƟ',
+		'P':'Ƥ¶',
+		'R':'ŔŘɌⱤ',
+		'S':'ßſŚŜŞṢŠÞ§',
+		'T':'ŢŤṮƬƮ',
+		'U':'ÙÚÛÜŪŬŮŰŲɄƯƱ',
+		'V':'Ʋ',
+		'W':'ŴẄΩ',
+		'Y':'ÝŶŸƔƳ',
+		'Z':'ŹŻŽƵƷẔ',
 		
 		// lower
-		'a': 'āăàáâãäåąɑæαª',
-		'b': 'ßβɓ',
-		'c': 'çςćĉċč¢ɔ',
-		'd': 'ðďđɖḏɖɗ',
-		'e': 'èéêëēėęẹěəæεɛ€',
-		'f': 'ƒʃƭ',
-		'g': 'ĝğġģɠƣ',
-		'h': 'ĥħɦẖ',
-		'i': 'ìíîïīįịiiɨĳι',
-		'j': 'ĵɟĳ',
-		'k': 'ķƙ',
-		'l': 'ĺļľłλ',
-		'n': 'ñńņňŋɲ',
-		'o': 'òóôõöōøőœơɵ°',
-		'p': 'ƥ¶',
-		'r': 'ŕřɍɽ',
-		's': 'ßſśŝşṣšþ§',
-		't': 'ţťṯƭʈ',
-		'u': 'ùúûüūŭůűųưμυʉʊ',
-		'v': 'ʋ',
-		'w': 'ŵẅω',
-		'y': 'ýŷÿɣyƴ',
-		'z': 'źżžƶẕʒƹ',
+		'a':'āăàáâãäåąɑæαª',
+		'b':'ßβɓ',
+		'c':'çςćĉċč¢ɔ',
+		'd':'ðďđɖḏɖɗ',
+		'e':'èéêëēėęẹěəæεɛ€',
+		'f':'ƒʃƭ',
+		'g':'ĝğġģɠƣ',
+		'h':'ĥħɦẖ',
+		'i':'ìíîïīįịiiɨĳι',
+		'j':'ĵɟĳ',
+		'k':'ķƙ',
+		'l':'ĺļľłλ',
+		'n':'ñńņňŋɲ',
+		'o':'òóôõöōøőœơɵ°',
+		'p':'ƥ¶',
+		'r':'ŕřɍɽ',
+		's':'ßſśŝşṣšþ§',
+		't':'ţťṯƭʈ',
+		'u':'ùúûüūŭůűųưμυʉʊ',
+		'v':'ʋ',
+		'w':'ŵẅω',
+		'y':'ýŷÿɣyƴ',
+		'z':'źżžƶẕʒƹ',
 
 		// Misc
-		'$': '£¥€₩₨₳Ƀ₹¤',
-		'!': '¡‼‽',
-		'?': '¿‽',
-		'%': '‰',
-		'.': '…••',
-		'-': '±‐–—',
-		'+': '±†‡',
-		'\'': '′″‴‘’‚‛',
-		'"': '“”„‟',
-		'<': '≤‹',
-		'>': '≥›',
-		'=': '≈≠≡'
+		'$':'£¥€₩₨₳Ƀ₹¤',
+		'!':'¡‼‽',
+		'?':'¿‽',
+		'%':'‰',
+		'.':'…••',
+		'-':'±‐–—',
+		'+':'±†‡',
+		'\'':'′″‴‘’‚‛',
+		'"':'“”„‟',
+		'<':'≤‹',
+		'>':'≥›',
+		'=':'≈≠≡'
 		
 	};
-	var ignoredKeys = [8, 13, 37, 38, 39, 40];
+	var ignoredKeys=[8, 13, 37, 38, 39, 40];
 
 	var selectedCharIndex;
 	var lastWhich;
 	var timer;
 	var activeElement;
 
-	var popup = $('<ul class=long-press-popup />');
+	var popup=$('<ul class=long-press-popup />');
 
 	$(window).mousewheel(onWheel);
 	$(window).keyup(onKeyUp);
@@ -105,36 +104,36 @@
 	function onKeyDown(e) {
 
 		// Arrow key with popup visible
-		if ($('.long-press-popup').length > 0 && (e.which == 37 || e.which == 39)) {
-			if (e.which == 37) activePreviousLetter();
-			else if (e.which == 39) activateNextLetter();
+		if ($('.long-press-popup').length>0 && (e.which==37 || e.which==39)) {
+			if (e.which==37) activePreviousLetter();
+			else if (e.which==39) activateNextLetter();
 
 			e.preventDefault();
 			return;
 		}
 
-		if (ignoredKeys.indexOf(e.which) > -1) return;
-		activeElement = e.target;
+		if (ignoredKeys.indexOf(e.which)>-1) return;
+		activeElement=e.target;
 
-		if (e.which == lastWhich) {
+		if (e.which==lastWhich) {
 			e.preventDefault();
-			if (!timer) timer = setTimeout(onTimer, 10);
+			if (!timer) timer=setTimeout(onTimer, 10);
 			return;
 		}
-		lastWhich = e.which;
+		lastWhich=e.which;
 	}
 	function onKeyUp(e) {
-		if (ignoredKeys.indexOf(e.which) > -1) return;
-		if (activeElement == null) return;
+		if (ignoredKeys.indexOf(e.which)>-1) return;
+		if (activeElement==null) return;
 
-		lastWhich = null;
+		lastWhich=null;
 		clearTimeout(timer);
-		timer = null;
+		timer=null;
 
 		hidePopup();
 	}
 	function onTimer() {
-		var typedChar = $(activeElement).val().split('')[getCaretPosition(activeElement) - 1];
+		var typedChar=$(activeElement).val().split('')[getCaretPosition(activeElement)-1];
 
 		if (moreChars[typedChar]) {
 			showPopup((moreChars[typedChar]));
@@ -145,19 +144,19 @@
 	function showPopup(chars) {
 		popup.empty();
 		var letter;
-		for (var i = 0; i < chars.length; i++) {
-			letter = $('<li class=long-press-letter />').text(chars[i]);
+		for (var i=0; i<chars.length; i++) {
+			letter=$('<li class=long-press-letter />').text(chars[i]);
 			letter.mouseenter(activateLetter);
 			popup.append(letter);
 		}
 		$('body').append(popup);
-		selectedCharIndex = -1;
+		selectedCharIndex=-1;
 	}
 	function activateLetter(e) {
 		selectCharIndex($(e.target).index());
 	}
 	function activateRelativeLetter(i) {
-		selectCharIndex(($('.long-press-letter').length + selectedCharIndex + i) % $('.long-press-letter').length);
+		selectCharIndex(($('.long-press-letter').length+selectedCharIndex+i) % $('.long-press-letter').length);
 	}
 	function activateNextLetter() {
 		activateRelativeLetter(1);
@@ -169,51 +168,51 @@
 		popup.detach();
 	}
 	function onWheel(e, delta, deltaX, deltaY) {
-		if ($('.long-press-popup').length == 0) return;
+		if ($('.long-press-popup').length==0) return;
 		e.preventDefault();
-		delta < 0 ? activateNextLetter() : activePreviousLetter();
+		delta<0 ? activateNextLetter() : activePreviousLetter();
 	}
 	function selectCharIndex(i) {
 		$('.long-press-letter.selected').removeClass('selected');
 		$('.long-press-letter').eq(i).addClass('selected');
-		selectedCharIndex = i;
+		selectedCharIndex=i;
 		updateChar();
 	}
 
 	function updateChar() {
-		var newChar = $('.long-press-letter.selected').text();
-		var pos = getCaretPosition(activeElement);
-		var arVal = $(activeElement).val().split('');
-		arVal[pos - 1] = newChar;
+		var newChar=$('.long-press-letter.selected').text();
+		var pos=getCaretPosition(activeElement);
+		var arVal=$(activeElement).val().split('');
+		arVal[pos-1]=newChar;
 		$(activeElement).val(arVal.join(''));
 		setCaretPosition(activeElement, pos);
 	}
 
-	function LongPress(element, options) {
+    function LongPress( element, options ) {
 
-		this.element = element;
-		this.options = $.extend({}, defaults, options);
+        this.element = element;
+		this.options = $.extend( {}, defaults, options) ;
         
-		this._defaults = defaults;
-		this._name = pluginName;
+        this._defaults = defaults;
+        this._name = pluginName;
         
-		this.init();
-	}
+        this.init();
+    }
 
 	LongPress.prototype = {
 
 		init: function () {
 			$(this.element).keydown(onKeyDown);
-		}
+        }
 
 	};
 
-	$.fn[pluginName] = function (options) {
-		return this.each(function () {
-			if (!$.data(this, 'plugin_' + pluginName)) {
-				$.data(this, 'plugin_' + pluginName, new LongPress(this, options));
-			}
-		});
-	};
+    $.fn[pluginName] = function ( options ) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, new LongPress( this, options ));
+            }
+        });
+    };
 
 });
